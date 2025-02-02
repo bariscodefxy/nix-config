c7d6514ea6fcb888e033b7c66b2d4861e4294e66
bariscodefx <root@bariscodefx.com.tr>
2025-02-02 18:10:01 +0300

    git: made credential.helper = "store";

--- home-manager/programs/git/default.nix
+++ home-manager/programs/git/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.git = {
     enable = true;
     userName = "bariscodefx";
     userEmail = "root@bariscodefx.com.tr";
     extraConfig = {
-      credential.helper = "${pkgs.git.override { withLibsecret = true; }}/bin/git-credential-libsecret";
+      credential.helper = "store";
     };
   };
 }
