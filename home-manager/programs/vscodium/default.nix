9abbf67c2d35deb5840d6816d0914a597ae20bbb
bariscodefx <root@bariscodefx.com.tr>
2025-02-05 01:02:32 +0300

    vscodium: added some extensions and changed settings
    Added extensions:
    ```
    devsense.phptools-vscode
    ms-python.python
    formulahendry.auto-rename-tag
    formulahendry.auto-close-tag
    rifi2k.format-html-in-php
    ms-vscode.sublime-keybindings
    ```
    
    Added settings:
    ```
    "update.mode" = "none";
    "extensions.autoUpdate" = false;
    ```

--- home-manager/programs/vscodium/default.nix
+++ home-manager/programs/vscodium/default.nix
@@ -10,15 +10,38 @@
     enable = true;
     package = pkgs.vscodium;
 
-    extensions = with pkgs.vscode-extensions; [
-      jnoortheen.nix-ide
-      arrterian.nix-env-selector
-      ms-python.python
-      ms-vscode.cpptools
-      leonardssh.vscord
-    ];
+    extensions =
+      with pkgs.vscode-extensions;
+      [
+        jnoortheen.nix-ide
+        arrterian.nix-env-selector
+        ms-python.python
+        ms-vscode.cpptools
+        leonardssh.vscord
+        devsense.phptools-vscode
+        ms-python.python
+        formulahendry.auto-rename-tag
+        formulahendry.auto-close-tag
+      ]
+      ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          publisher = "rifi2k";
+          name = "format-html-in-php";
+          version = "1.7.0";
+          sha256 = "sha256-2o8bfCHygTLLzucoiq4h8DfgCKUlj0U7aV8vZr0XuDY=";
+        }
+        {
+          publisher = "ms-vscode";
+          name = "sublime-keybindings";
+          version = "4.1.10";
+          sha256 = "sha256-XlogenuBmP+tE18VLH4lUSpOq/7d022n8HgXnKjY3n0=";
+        }
+      ];
 
     userSettings = {
+      "update.mode" = "none";
+      "extensions.autoUpdate" = false;
+
       "editor.fontFamily" = "JetBrainsMono Nerd Font";
       "terminal.integrated.fontFamily" = "JetBrainsMono Nerd Font";
       "editor.fontSize" = 18;
