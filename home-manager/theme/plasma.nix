d5426e4ed8d6b686f484b7f1368734a8f856504c
bariscodefx <root@bariscodefx.com.tr>
2025-02-11 03:02:08 +0300

    home: theme: remove plasma.nix

--- home-manager/theme/plasma.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-{
-  programs.plasma.workspace = {
-    lookAndFeel = "com.github.vinceliuice.WhiteSur-dark";
-    wallpaper = "${pkgs.whitesur-kde}/share/wallpapers/WhiteSur-dark/contents/images/3840x2160.jpg";
-  };
-}
