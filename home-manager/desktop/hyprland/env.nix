1644fb941b22929fd1288e27a893cf26a7f80bd3
bariscodefx <root@bariscodefx.com.tr>
2025-02-11 11:10:04 +0300

    home: desktop: hyprland: env: fix EDITOR

--- home-manager/desktop/hyprland/env.nix
+++ home-manager/desktop/hyprland/env.nix
@@ -28,7 +28,7 @@
     CLUTTER_BACKEND = "wayland";
     LIBVA_DRIVER_NAME = "nvidia";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-    EDITOR = "vscodium";
+    EDITOR = "codium";
     BROWSER = "floorp";
     TERMINAL = "kitty";
   };
