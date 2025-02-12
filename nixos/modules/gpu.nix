ab7d2765bfeb53350d868d011403d82cce7620f5
bariscodefx <root@bariscodefx.com.tr>
2025-02-12 05:40:27 +0300

    nixos: modules: gpu: switch nvidia channel from beta to stable

--- nixos/modules/gpu.nix
+++ nixos/modules/gpu.nix
@@ -15,6 +15,6 @@
     powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
