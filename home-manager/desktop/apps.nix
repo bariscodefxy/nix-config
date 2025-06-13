346082f66f4c954eb8e25210bc53f9a39973b75e
bariscodefx <root@bariscodefx.tr>
2025-06-13 20:52:39 +0300

    feat: add gnome core apps for be sure they're installed

---
+++ home-manager/desktop/apps.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+{
+  home.packages = with pkgs; [
+    # Core GNOME applications
+    nautilus
+    gnome-terminal
+    gedit
+    gnome-calculator
+    gnome-calendar
+    gnome-clocks
+    gnome-weather
+    gnome-maps
+    gnome-contacts
+    gnome-music
+    gnome-photos
+    totem
+    evince
+    gnome-software
+    gnome-system-monitor
+    gnome-disk-utility
+    gnome-font-viewer
+    gnome-screenshot
+    gnome-logs
+  ];
+}
--- home-manager/desktop/default.nix
+++ home-manager/desktop/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   imports = [
+    ./apps.nix
     ./theme.nix
   ];
 }
