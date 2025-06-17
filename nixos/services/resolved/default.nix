15f0d08bf0ae4663977558e44068c6f60f9d5121
bariscodefx <root@bariscodefx.tr>
2025-06-17 14:42:50 +0300

    feat: update nameservers and remove fallback DNS configuration

--- nixos/services/resolved/default.nix
+++ nixos/services/resolved/default.nix
@@ -1,13 +1,15 @@
 { ... }:
 {
-  networking.nameservers = [ "77.90.5.73#adguard.bariscodefx.tr" ];
-  #networking.nameservers = [ "1.1.1.1#one.one.one.one" "1.0.0.1#one.one.one.one" ];
+  networking.nameservers = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
 
   services.resolved = {
     enable = true;
     dnssec = "true";
     domains = [ "~." ];
-    fallbackDns = [ "77.90.5.73#adguard.bariscodefx.tr" ];
+    fallbackDns = [ ];
     dnsovertls = "true";
   };
 }
