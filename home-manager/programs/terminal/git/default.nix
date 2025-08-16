ded84adcc4fc8b3184d9f01a3790d245177a967c
bariscodefx <root@bariscodefx.tr>
2025-08-16 17:05:53 +0300

    fix: git: added postBuffer to the config for push issue in big repositories

--- home-manager/programs/terminal/git/default.nix
+++ home-manager/programs/terminal/git/default.nix
@@ -7,6 +7,12 @@
     userEmail = "root@bariscodefx.tr";
     extraConfig = {
       credential.helper = "store";
+      http = {
+        postBuffer = 157286400;
+      };
+      https = {
+        postBuffer = 157286400;
+      };
     };
   };
 }
