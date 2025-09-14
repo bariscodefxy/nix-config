4ef87971464499e807117a2a3d3023de36c93cf1
bariscodefx <root@bariscodefx.tr>
2025-09-14 23:32:41 +0300

    nixos: modules: boot: remove vmware host modules for workstation comment line

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -5,9 +5,6 @@
   boot.extraModulePackages = [
     config.hardware.nvidia.package
     config.boot.kernelPackages.v4l2loopback
-
-    # (WIP)
-    # pkgs.vmware-host-modules-workstation-17-6-0
   ];
 
   boot.loader = {
