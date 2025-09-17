e06913d0644b6b6a443dcc3f15721ed4a62d4309
bariscodefx <root@bariscodefx.tr>
2025-09-17 15:32:16 +0300

    home-manager: programs: remove lxqt panel restart script

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -10,16 +10,4 @@
     ./programming
     ./terminal
   ];
-
-  home.activation.restartLxqtPanel = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    PGREP=${pkgs.procps}/bin/pgrep
-    PKILL=${pkgs.procps}/bin/pkill
-    LXQT_PANEL=${pkgs.lxqt.lxqt-panel}/bin/lxqt-panel
-
-    if $PGREP lxqt-panel > /dev/null; then
-      echo "Restarting lxqt-panel..."
-      $PKILL lxqt-panel
-      nohup $LXQT_PANEL > /dev/null 2>&1 &
-    fi
-  '';
 }
