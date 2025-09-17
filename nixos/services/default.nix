20b87051a56e15a05ef8972cfd65c1dabf883f90
bariscodefx <root@bariscodefx.tr>
2025-09-17 15:33:29 +0300

    nixos: modules: add specialisations for some services

--- nixos/services/default.nix
+++ nixos/services/default.nix
@@ -1,13 +1,32 @@
 { ... }:
 {
   imports = [
-    ./dnscrypt.nix
     ./flatpak.nix
-    #./mullvad.nix
+    ./mullvad.nix
     ./openssh.nix
     ./pipewire.nix
     ./xscreensaver.nix
-    ./redsocks.nix
-    ./zapret.nix
   ];
+
+  specialisation = {
+    "redsocks" = {
+      inheritParentConfig = true;
+      configuration = { config, pkgs, ... }: {
+        system.nixos.tags = [ "spec-redsocks" ];
+        imports = [
+          ./redsocks.nix
+        ];
+      };
+    };
+    "zapret" = {
+      inheritParentConfig = true;
+      configuration = { config, pkgs, ... }: {
+        system.nixos.tags = [ "spec-zapret" ];
+        imports = [
+          ./dnscrypt.nix
+          ./zapret.nix
+        ];
+      };
+    };
+  };
 }
