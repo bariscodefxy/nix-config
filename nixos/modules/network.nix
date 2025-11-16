4f4b26d9f3c2af74f31aee72edc864a48d5c5323
bariscodefx <root@bariscodefx.tr>
2025-11-17 01:38:13 +0300

    feat: comment out proxy configuration in network settings

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -3,5 +3,9 @@
   networking = {
     hostName = "victus";
     networkmanager.enable = true;
+    # proxy = {
+    #   default = "socks5://10.205.71.246:1080/";
+    #   noProxy = "127.0.0.1,localhost,internal.domain";
+    # };
   };
 }
