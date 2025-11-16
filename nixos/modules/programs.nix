728e824b1f57a7fb5ae6ab83345ae06df87df561
bariscodefx <root@bariscodefx.tr>
2025-11-16 13:07:36 +0300

    feat: add bottles to system packages in programs.nix

--- nixos/modules/programs.nix
+++ nixos/modules/programs.nix
@@ -1,6 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zsh.enable = true;
   programs.nix-ld.enable = true;
   programs.nm-applet.enable = true;
+  environment.systemPackages = with pkgs; [
+    bottles
+  ];
 }
