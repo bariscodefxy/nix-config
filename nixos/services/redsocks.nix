ec287133fdeac77535ed2504c5555f44a0dfb0db
bariscodefx <root@bariscodefx.tr>
2025-11-17 01:38:42 +0300

    fix: update proxy address in redsocks configuration

--- nixos/services/redsocks.nix
+++ nixos/services/redsocks.nix
@@ -8,7 +8,7 @@
           "-d 172.21.0.0/16"
         ];
         port = 23456;
-        proxy = "172.21.48.159:1080";
+        proxy = "10.205.71.246:1080";
         redirectCondition = true;
         type = "socks5";
       }
