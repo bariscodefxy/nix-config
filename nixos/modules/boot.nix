3372cb2f156c160e50e771764ec0b7fd0897752f
bariscodefx <root@bariscodefx.tr>
2025-11-17 01:38:03 +0300

    feat: update graphics configuration to include NVIDIA settings and enable hardware acceleration

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -3,7 +3,6 @@
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.kernelParams = [ "nvidia.NVreg_TemporaryFilePath=/var/tmp" ];
   boot.extraModulePackages = [
-    config.hardware.nvidia.package
     config.boot.kernelPackages.v4l2loopback
   ];
 
--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -1,13 +1,35 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      # For modern Intel CPU's
-      intel-media-driver # Enable Hardware Acceleration
-      vpl-gpu-rt # Enable QSV
-    ];
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    nvidia = {
+      modesetting.enable = true;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.production;
+
+      powerManagement = {
+        enable = true;
+        finegrained = true;
+      };
+
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+
+        intelBusId = "PCI:0:2:0";
+        nvidiaBusId = "PCI:1:0:0";
+      };
+    };
   };
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
+
+  services.xserver.videoDrivers = [
+    "modesetting"
+    "nvidia"
+  ];
 }
