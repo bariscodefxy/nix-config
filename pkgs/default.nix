1cdf94e98c93655fdb7e7c3a9d04207fc326360a
bariscodefx <root@bariscodefx.tr>
2025-11-17 01:38:37 +0300

    feat: remove VMware host modules configuration from default packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,1 @@
-pkgs: {
-  vmware-host-modules-workstation-17-6-0 =
-    pkgs.callPackage ./vmware-host-modules-workstation-17.6.0.nix
-      {
-        kernel = pkgs.linuxPackages_zen.kernel;
-      };
-}
+pkgs: { }
