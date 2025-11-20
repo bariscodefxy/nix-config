6591fcdba27dca2b04b84e3c7f92137d0a5c6730
bariscodefx <root@bariscodefx.tr>
2025-11-20 17:31:16 +0300

    networking: firewall: custom firewall

--- nixos/modules/network.nix
+++ nixos/modules/network.nix
@@ -8,4 +8,27 @@
     #   noProxy = "127.0.0.1,localhost,internal.domain";
     # };
   };
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      80
+      443
+      8080
+      3000
+      5000
+      8888
+      5173
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 4000;
+        to = 4007;
+      }
+      {
+        from = 8000;
+        to = 8010;
+      }
+    ];
+  };
 }
