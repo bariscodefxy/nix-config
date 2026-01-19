d4016cfaf25b83ec9932f92f50d3e3aa67739a06
bariscodefx <root@bariscodefx.tr>
2026-01-19 03:16:20 +0300

    nixos: update boot kernel parameters and enable VirtualBox support

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,7 +1,10 @@
 { pkgs, config, ... }:
 {
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.kernelParams = [ "nvidia.NVreg_TemporaryFilePath=/var/tmp" ];
+  boot.kernelParams = [
+    "nvidia.NVreg_TemporaryFilePath=/var/tmp"
+    "kvm.enable_virt_at_load=0"
+  ];
   boot.kernelModules = [ "tun" ];
   boot.extraModulePackages = [
     config.boot.kernelPackages.v4l2loopback
--- nixos/modules/virtualisation.nix
+++ nixos/modules/virtualisation.nix
@@ -3,4 +3,6 @@
 }:
 {
   virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "bariscodefx" ];
 }
